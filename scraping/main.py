570eb966a4add8eded194b5a236641b6de70ca6c
M Kawada <m@llll.jp>
2024-11-12 16:35:58 +0900

    ID,PWまでOK

--- scraping/main.py
+++ scraping/main.py
@@ -1,3 +1,5 @@
+from jwt.utils import bytes_to_number
+
 url="https://192.168.1.103:6150/?onepasswdfill=E393059F71954B619ED7E87023B37F10&onepasswdvault=FFDAE69715284BD8AC3CE978276A0485#/signin"
 ID="tablet"
 PW="Jyt=og"
@@ -21,29 +23,41 @@ sleep(2)
 login_button = driver.find_element(By.XPATH, value='//*[@id="details-button"]')
 login_button.click()
 
-
 sleep(2)
 login_button = driver.find_element(By.XPATH, value='//*[@id="proceed-link"]')
 login_button.click()
 
-//変更OK？
-
-
-sleep(2)
-fb_login = driver.find_element(By.XPATH, value='//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
-fb_login.click()
 
 sleep(2)
 base_window = driver.window_handles[0]
-fb_login_window = driver.window_handles[1]
-driver.switch_to.window(fb_login_window)
-print(driver.title)
+# fb_login_window = driver.window_handles[1]
+# driver.switch_to.window(fb_login_window)
+# print(driver.title)
 
-email = driver.find_element(By.XPATH, value='//*[@id="email"]')
-password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
-email.send_keys(FB_EMAIL)
-password.send_keys(FB_PASSWORD)
-password.send_keys(Keys.ENTER)
+
+
+IDinput = driver.find_element(By.XPATH, value='//*[@id="dsm-user-fieldset"]/div/div/div[1]/input')
+# password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
+IDinput.send_keys(ID)
+# password.send_keys(PW)
+# password.send_keys(Keys.ENTER)
+login_button = driver.find_element(By.XPATH, value='//*[@id="sds-login-vue-inst"]/div/span/div/div[2]/div[2]/div/div[3]/div[2]/div/div[2]/div[3]/div[1]')
+login_button.click()
+
+
+
+sleep(2)
+PWinput = driver.find_element(By.XPATH, value='//*[@id="dsm-pass-fieldset"]/div[1]/div/div[1]/input')
+# password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
+PWinput.send_keys(PW)
+# password.send_keys(PW)
+# password.send_keys(Keys.ENTER)
+login_button = driver.find_element(By.XPATH, value='//*[@id="sds-login-vue-inst"]/div/span/div/div[2]/div[2]/div/div[3]/div[2]/div/div[2]/div[1]/div[3]/div[1]')
+login_button.click()
+# ここまでOK
+
+
+
 
 driver.switch_to.window(base_window)
 print(driver.title)
